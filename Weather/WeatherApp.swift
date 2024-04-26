bf3d54203371b730678467fca0fa520348a18ee5
vladyslav-ios <vlad.lyalkin@icloud.com>
2024-04-26 22:01:30 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Влад Лялькін on 26.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
